1f3eae34241e911e7af3c49f4002296c5c51e246
akash <akash.kushwaha02@gmail.com>
2021-03-09 15:27:00 +0530

    added category model

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,18 @@ from django.urls import reverse
 
 
 # Create your models here.
+
+class BlogCategory(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name +' | '
+
+    def get_absolute_url(self):
+        #return reverse('details', args=(str(self.id)))
+        return reverse('home')
+
+
 class Blog(models.Model):
     title = models.CharField(max_length=100)
     title_tag = models.CharField(max_length=100)
@@ -11,6 +23,7 @@ class Blog(models.Model):
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     image = models.ImageField(upload_to='blog/images',blank=True,null=True)
+    category = models.CharField(max_length=100,default='uncategorized')
 
     def __str__(self):
         return self.title +' | '
